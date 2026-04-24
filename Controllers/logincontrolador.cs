9f1c5fce4a062cc322051f1bb7bbe5a084a42c4d
laura <riveroslaura277@gmail.com>
2026-04-24 17:25:59 -0500

    cree index login y controlador login

---
+++ Controllers/logincontrolador.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduClick.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string correo, string clave)
+        {
+            if (correo == "admin@educlick.com" && clave == "1234")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Correo o contraseña incorrectos";
+            return View();
+        }
+    }
+}
