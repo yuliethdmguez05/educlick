35dd450f062667643d806e1a524b12a17fce775e
laura <riveroslaura277@gmail.com>
2026-04-24 15:47:09 -0500

    nueva rama

---
+++ Controllers/SecretariaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduClick.Controllers
+{
+    public class SecretariaController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
