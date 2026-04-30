824438a17373f9283cbd75fcc164403c0d10ac4e
laura <riveroslaura277@gmail.com>
2026-04-29 22:54:39 -0500

    cambio secretaria

---
+++ Controllers/Usuarios.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduClick.Controllers
+{
+    public class Usuarios : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
