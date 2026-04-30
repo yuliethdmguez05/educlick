240ab2a2298e88603a15d0c57f0686e8eab8cecc
laura <riveroslaura277@gmail.com>
2026-04-29 21:42:46 -0500

    ok

---
+++ Controllers/ADMINISTRATIVO/SECRETARIA/SecretariaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduClick.Controllers.ADMINISTRATIVO.SECRETARIA
+{
+    public class SecretariaController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
